5ebaeb72cb68077a14e882b6cc0d8dcff3542b9b
Bushra Javed <39137046+bushrajav@users.noreply.github.com>
2019-03-27 18:38:51 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Practice from './Todolist/Practice';
+import Todolist from './Todolist/Todolist';
+
+
+class App extends Component {
+	 
+render(){
+	return (
+	
+	<div>
+	
+<Practice/>
+</div>
+	);
+
+}
+}
+
+export default App;
+
+	
